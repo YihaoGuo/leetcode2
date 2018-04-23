ade6d04a0326e10ad649706eb394264c915f3350
郭益豪 <guoyihao@guoyihaodeMacBook-Pro.local>
2018-04-23 16:29:33 +0800

    1

---
+++ leetcode2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  leetcode2
+//
+//  Created by 郭益豪 on 2018/4/23.
+//  Copyright © 2018年 郭益豪. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
